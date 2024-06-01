374e3af73fa7a3487f1f7f4785766eab3c1aac88
Heritage <hrtg.clothing@gmail.com>
2024-06-01 18:33:35 -0500

    14 Commit - Final Version

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
-  webpack: (config) => {
+  webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
@@ -17,7 +17,6 @@ const nextConfig = {
   images: {
     domains: ['ipfs.io'],
   },
-  distDir: 'out',  // Esto asegura que el directorio de salida sea 'out'
 };
 
 module.exports = nextConfig;
